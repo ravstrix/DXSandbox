280036d39f4cc49e233d18d16375ccb84ae02b26
Alexander Ryabykin <ravstrix@yandex.ru>
2023-10-09 00:04:58 +0300

    Turn on debug allocation and memory leak check at program exit

--- DXSandbox/EntryPoint.cpp
+++ DXSandbox/EntryPoint.cpp
@@ -2,9 +2,17 @@
 
 #include "Application.hpp"
 
+#ifndef NDEBUG
+#   include <crtdbg.h>
+#endif
+
 _Use_decl_annotations_
 int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int)
 {
+#ifndef NDEBUG
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_CHECK_ALWAYS_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
     DXSandbox::Application app{hInstance};
 
     return app.Run();
